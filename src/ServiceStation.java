f254042d33623ba186140a0d80de3e294771addd
Verflog <pnbaranova@gmail.com>
2025-02-03 20:51:12 +0300

    HW-15

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,23 +1,21 @@
 public class ServiceStation {
-    public void check(Car car, Bicycle bicycle, Truck truck) {
-        if (car != null) {
-            System.out.println("Обслуживаем " + car.modelName);
-            for (int i = 0; i < car.wheelsCount; i++) {
-                car.updateTyre();
+    public void check(Transport transport) {
+        if (transport != null) {
+            System.out.println("Обслуживаем " + transport.getModelName());
+            for (int i = 0; i < transport.getWheelsCount(); i++) {
+                transport.updateTyre();
             }
-            car.checkEngine();
-        } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
+        }
+        if (transport != null && transport.getClass().equals(Car.class) | transport.getClass().equals(Truck.class)) {
+            ((Car) transport).checkEngine();
+        } else if (transport != null && transport.getClass().equals(Bicycle.class)) {
+            System.out.println("Обслуживание завершено");
+        }
+        if (transport != null && transport.getClass().equals(Truck.class)) {
+            ((Truck) transport).checkTrailer();
+            System.out.println("Обслуживание завершено");
+        } else if (transport != null && transport.getClass().equals(Car.class)) {
+            System.out.println("Обслуживание завершено");
         }
     }
 }
